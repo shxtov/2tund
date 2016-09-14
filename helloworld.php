fc34e4bbcf743f0e4c90b53eebdb045dcaf93741
vladsuto <vladsuto@greeny.cs.tlu.ee>
2016-09-14 09:34:47 +0300

    tsuklid

--- helloworld.php
+++ helloworld.php
@@ -1,4 +1,5 @@
 <?php
+
 	// $ - pridaem znachenie
 
 	$firstName = "Vlad";
@@ -6,12 +7,56 @@
 	
 	// echo trükib välja muutuja väärtuse
 	
-	echo $firstName;
-	echo $lastName;
+	//echo $firstName;
+	//echo $lastName;
 	
 	//echo s probelom (ispolzujutsa . dlja "slozhenija" string)
 	
 	echo $firstName." ".$lastName;
 	
+?>
+
+<br>
+
+<?php
+
+	$age = 5;
+	
+	if ($age < 18){
+		
+		//kui vastus on tõene
+		echo "alaealine";
+		
+	} else {
+		
+		//kui vastus on väär
+		echo "täisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php
+
+ 	//tsüklid for() - kolm asja sulgudesse 1. enne 1 korda ($i = 0), 2. enne igat korda ($i < $age), 3. pärast igat korda ($i = $i + 1)
+	
+	for($i = 1; $i <= $age; $i = $i + 1){
+		
+		echo $i."palju ";
+		
+		
+	}
+	
+	echo "õnne!"
+
+?>
+
+<br>
+
+<?php
+
+	echo date("l, d M Y. H:i:s");
 
 ?>
