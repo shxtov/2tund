e47974d0c10fdf72f1ccf4b306fc02789be078b7
vladsuto <vladsuto@greeny.cs.tlu.ee>
2016-09-14 08:53:38 +0300

    variables and echo

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,15 @@
 <?php
+	// $ - pridaem znachenie
 
-
+	$firstName = "Vlad";
+	$lastName = "Šutov";
+	
+	// echo trükib välja muutuja väärtuse
+	
+	echo $firstName;
+	echo $lastName;
+	
+	echo $firstName." ".$lastName;
+	
 
 ?>
